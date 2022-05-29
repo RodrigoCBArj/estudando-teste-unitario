f48049e5a4df06e34cf81f6084b9d53d29cc3741
RodrigoCBArj <rodrigocbarj@gmail.com>
2022-05-29 12:05:46 -0300

    Testando com Mock/Mockito

--- junit4/src/test/java/br/com/rodrigocbarj/CalculatorTest.java
+++ junit4/src/test/java/br/com/rodrigocbarj/CalculatorTest.java
@@ -1,16 +1,30 @@
 package br.com.rodrigocbarj;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 
 public class CalculatorTest {
-    Calculator calculator = new Calculator();
-
+    
     @Test
     public void testSum() {
+        Calculator calculator = new Calculator();
         
         int sumTest01 = calculator.sum("1+2+3");
+
         assertEquals(6, sumTest01);
+    }
+
+    @Test
+    public void testSumMock() { // teste utilizando Mock/Mockito
+        Calculator calculator = mock(Calculator.class);
+
+        when(calculator.sum("1+2")).thenReturn(10);
+        
+        int result = calculator.sum("1+2");
+
+        assertEquals(10, result);
     }
 }
